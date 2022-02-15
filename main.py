71bf8b53668c4331523093a1d65a4eab2251e9d2
kaptsov <kaptsovaa@gmail.com>
2022-02-15 22:30:46 +0400

    changed defs and main

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def predict_rub_salary_sj(vacancy):
     salary_from = vacancy['payment_from']
     salary_to = vacancy['payment_to']
     salary_currency = vacancy['currency']
-
     if salary_currency == 'rub':
         return predict_salary(salary_from, salary_to)
 
@@ -33,106 +32,86 @@ def predict_rub_salary_hh(vacancy):
         return predict_salary(salary_from, salary_to)
 
 
-def fetch_headhunter(lang_collection):
+def get_headhunter_vacancy(language):
 
-    language_salaries = {}
+    average_salary = 0
+    page = 0
+    pages_amount = 1
+    per_page = 100
 
-    for lang in lang_collection:
+    while page < pages_amount:
+        vacancy_search_field = {
+            'text': f'программист {language}',
+            'area': 1,
+            'date_from': '2022-02-01',
+            'per_page': per_page,
+            'page': page,
+            'only_with_salary': True,
+            'salary': 70000,
 
-        ave_salary = 0
-        page = 0
-        pages_amount = 1
-        per_page = 100
-
-        while page < pages_amount:
-            vacancy_search_field = {
-                'text': f'программист {lang}',
-                'area': 1,
-                'date_from': '2022-02-01',
-                'per_page': per_page,
-                'page': page,
-                'only_with_salary': True,
-                'salary': 70000,
-
-            }
-            response = requests.get('https://api.hh.ru/vacancies',
-                                    params=vacancy_search_field)
-            response.raise_for_status()
-            pages_amount = response.json()['pages']
-            vacancies_found = int(response.json()['found'])
-
-            for vacancy in response.json()['items']:
-                if vacancy['salary'] and predict_rub_salary_hh(vacancy):
-                    ave_salary += predict_rub_salary_hh(vacancy)
-            page += 1
-        if vacancies_found > 2000:
-            vacancies_processed = pages_amount * per_page
-        else:
-            vacancies_processed = vacancies_found
-
-        ave_salary = int(ave_salary / vacancies_processed)
-
-        language_salaries[lang] = {
-            'vacancies_found': vacancies_found,
-            'vacancies_processed': vacancies_processed,
-            'average_salary': ave_salary,
         }
+        response = requests.get('https://api.hh.ru/vacancies',
+                                params=vacancy_search_field)
+        response.raise_for_status()
+        response_json = response.json()
+        pages_amount = response_json['pages']
+        vacancies_found = int(response_json['found'])
 
-    print_table(language_salaries, 'HeadHunter')
+        for vacancy in response_json['items']:
+            if vacancy['salary'] and predict_rub_salary_hh(vacancy):
+                average_salary += predict_rub_salary_hh(vacancy)
+        page += 1
+    if vacancies_found > 2000:
+        vacancies_processed = pages_amount * per_page
+    else:
+        vacancies_processed = vacancies_found
+
+    average_salary = int(average_salary / vacancies_processed)
+
+    return vacancies_found, vacancies_processed, average_salary
 
 
-def fetch_superjob(lang_collection):
+def get_superjob_vacancy(language, superjob_token):
 
-    superjob_token = os.getenv("SUPERJOB_SECRET_KEY")
     count = 100
-    language_salaries_sj = {}
 
     headers = {
         'X-Api-App-Id': superjob_token,
     }
-    for lang in lang_collection:
 
-        ave_salary = 0
-        min_sal = 10000000
-        max_sal = 0
-        page = 0
-        pages_amount = 1
+    average_salary = 0
+    page = 0
+    pages_amount = 1
 
-        while page < pages_amount:
+    while page < pages_amount:
 
-            params = {
-                'keywords': f'Программист {lang}',
-                'town': 'Москва',
-                'no_agreement': 1,
-                'payment_from': 70000,
-                'count': count
-            }
-            response = requests.get(f'https://api.superjob.ru/2.2/vacancies/',
-                                    headers=headers, params=params)
-            response.raise_for_status()
-
-            vacancies_found = int(response.json()['total'])
-            pages_amount = int(vacancies_found / count + 1)
-
-            for vacancy in response.json()['objects']:
-                if predict_rub_salary_sj(vacancy):
-                    ave_salary += predict_rub_salary_sj(vacancy)
-            page += 1
-
-        if vacancies_found > 2000:
-            vacancies_processed = pages_amount * count
-        else:
-            vacancies_processed = vacancies_found
-
-        ave_salary = int(ave_salary / vacancies_processed)
-
-        language_salaries_sj[lang] = {
-            'vacancies_found': vacancies_found,
-            'vacancies_processed': vacancies_processed,
-            'average_salary': ave_salary
+        params = {
+            'keywords': f'Программист {language}',
+            'town': 'Москва',
+            'no_agreement': 1,
+            'payment_from': 70000,
+            'count': count
         }
+        response = requests.get(f'https://api.superjob.ru/2.2/vacancies/',
+                                headers=headers, params=params)
+        response.raise_for_status()
+        response_json = response.json()
+        vacancies_found = int(response_json['total'])
+        pages_amount = int(vacancies_found / count + 1)
 
-    print_table(language_salaries_sj, 'SuperJob')
+        for vacancy in response.json()['objects']:
+            if predict_rub_salary_sj(vacancy):
+                average_salary += predict_rub_salary_sj(vacancy)
+        page += 1
+
+    if vacancies_found > 2000:
+        vacancies_processed = pages_amount * count
+    else:
+        vacancies_processed = vacancies_found
+
+    average_salary = int(average_salary / vacancies_processed)
+
+    return vacancies_found, vacancies_processed, average_salary
 
 
 def print_table(stat_by_language, title):
@@ -145,9 +124,9 @@ def print_table(stat_by_language, title):
         ),
     )
     for name, info in stat_by_language.items():
-        table_data += (name, info['vacancies_found'],
-                       info['vacancies_processed'],
-                       info['average_salary']),
+        table_data += (name, info[0],
+                       info[1],
+                       info[2]),
     table_instance = AsciiTable(table_data, title)
     print(table_instance.table)
 
@@ -156,6 +135,17 @@ if __name__ == '__main__':
 
     load_dotenv()
     lang_collection = os.getenv('LANGUAGE_COLLECTION').split(', ')
+    superjob_token = os.getenv("SUPERJOB_SECRET_KEY")
+    language_salaries_hh = {}
+    language_salaries_sj = {}
 
-    fetch_superjob(lang_collection)
-    fetch_headhunter(lang_collection)
+    print('Выполняется поиск... Может занять около нескольких минут...')
+    for language in lang_collection:
+        language_salaries_hh[language] = get_headhunter_vacancy(language)
+        language_salaries_sj[language] = get_superjob_vacancy(
+            language,
+            superjob_token
+            )
+
+    print_table(language_salaries_hh, 'HeadHunter')
+    print_table(language_salaries_sj,  'SuperJob')
